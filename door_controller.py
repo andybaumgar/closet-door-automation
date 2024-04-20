89b537e7281b47e28af37246e7a1722b14871681
Andy B <andybaumgar@gmail.com>
2024-04-20 13:27:26 -0400

    Update door_controller.py

--- door_controller.py
+++ door_controller.py
@@ -1,37 +1,60 @@
 import pigpio
 import time
+import argparse
 
-# Constants
-step_pin = 9  # GPIO pin for the STEP signal
-dir_pin = 22  # GPIO pin for the DIRECTION signal
-target_speed_rpm = 60
-single_revolution_steps = 4000
-steps_per_second = target_speed_rpm / 60 * single_revolution_steps
 
-# Setup
-pi = pigpio.pi()
-if not pi.connected:
-    exit()
+def move(speed_rpm=60, direction=0, duration_seconds=2):
+    # Constants
+    step_pin = 9  # GPIO pin for the STEP signal
+    dir_pin = 22  # GPIO pin for the DIRECTION signal
+    single_revolution_steps = 4000
+    steps_per_second = speed_rpm / 60 * single_revolution_steps
 
-# Set up GPIO pins
-pi.set_mode(step_pin, pigpio.OUTPUT)
-pi.set_mode(dir_pin, pigpio.OUTPUT)
+    # Setup
+    pi = pigpio.pi()
+    if not pi.connected:
+        exit()
 
-# Set direction (1 for clockwise, 0 for counterclockwise)
-pi.write(dir_pin, 1)
+    # Set up GPIO pins
+    pi.set_mode(step_pin, pigpio.OUTPUT)
+    pi.set_mode(dir_pin, pigpio.OUTPUT)
 
-# Calculate pulse frequency based on desired speed
-pulse_frequency = int(steps_per_second)
+    # Set direction (1 for clockwise, 0 for counterclockwise)
+    pi.write(dir_pin, direction)
+    time.sleep(0.1)
 
-# Generate pulses
-pi.set_PWM_frequency(step_pin, pulse_frequency)
-pi.set_PWM_dutycycle(step_pin, 128)  # 50% duty cycle
+    # Calculate pulse frequency based on desired speed
+    pulse_frequency = int(steps_per_second)
 
-# Run motor for 5 seconds
-time.sleep(5)
+    # Generate pulses
+    pi.set_PWM_frequency(step_pin, pulse_frequency)
+    pi.set_PWM_dutycycle(step_pin, 128)  # 50% duty cycle
 
-# Stop motor
-pi.set_PWM_dutycycle(step_pin, 0)  # Stop sending pulses
+    # Run motor for 5 seconds
+    time.sleep(duration_seconds)
 
-# Cleanup
-pi.stop()
+    # Stop motor
+    pi.set_PWM_dutycycle(step_pin, 0)  # Stop sending pulses
+
+    # Cleanup
+    pi.stop()
+
+
+def main(args):
+    move(
+        speed_rpm=args.speed_rpm,
+        direction=args.direction,
+        duration_seconds=args.duration_seconds,
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Script to move servo motor.")
+
+    parser.add_argument("speed_rpm", type=float)
+    parser.add_argument("direction", type=int)
+    parser.add_argument("duration_seconds", type=float)
+
+    args = parser.parse_args()
+
+    main(args)
