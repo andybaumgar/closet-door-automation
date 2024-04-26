9d0a1407e14d4a6540a7aaeb1a63a51cfc5f8345
Andy B <andybaumgar@gmail.com>
2024-04-25 21:45:11 -0400

    Create pwm_test.py

---
+++ pwm_test.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+import time
+import argparse
+
+
+def pulse():
+    step_pin = 9
+    steps_per_second = 4000
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(step_pin, GPIO.OUT)
+
+    pulse_frequency = int(steps_per_second)
+
+    print(f"pulse_frequency: {pulse_frequency}")
+
+    pwm = GPIO.PWM(step_pin, pulse_frequency)
+    pwm.start(50)  # start PWM at 50% duty cycle
+
+    time.sleep(20)
+
+    print(f"Acceleration steps: {acceleration_steps}")
+
+    # for i in range(acceleration_steps):
+    #     # pwm.ChangeFrequency(int(current_frequency))
+
+    pwm.stop()  # Stop sending pulses
+
+    GPIO.cleanup()
+
+
+pulse()
